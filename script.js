19ebbd690cfa2df2803149a37ffc1a62682e1385
marasulanilesh <121786431+marasulanilesh@users.noreply.github.com>
2026-07-18 12:20:41 +0530

    350 px

--- script.js
+++ script.js
@@ -6,11 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
         button.addEventListener("click", () => {
             const targetTab = button.getAttribute("data-tab");
 
-            // Remove active system states
             tabButtons.forEach(btn => btn.classList.remove("active"));
             tabContents.forEach(content => content.classList.remove("active"));
 
-            // Set current target active indicators
             button.classList.add("active");
             const activePanel = document.getElementById(targetTab);
             if (activePanel) {
